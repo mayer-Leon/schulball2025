73508fe2e3fc325611ca4b36cf447d4bdeb09b6b
mayer-Leon <160941846+mayer-Leon@users.noreply.github.com>
2025-01-25 14:52:30 +0100

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,19 +1,19 @@
 function showMenu() {
-    let mobileMenu = document.getElementById("mobileMenu");
+    let mobileMenu = document.querySelectorAll(".mobileMenu")[0];
     mobileMenu.style.opacity = "1";
     mobileMenu.style.transition = "200ms";
     mobileMenu.style.pointerEvents = "all";
-    let blur = document.getElementById("blur");
+    let blur = document.querySelectorAll(".blur")[0];
     blur.style.backdropFilter = "blur(10px)";
     blur.style.pointerEvents = "all";
 }
 
 function closeMenu() {
-    let mobileMenu = document.getElementById("mobileMenu");
+    let mobileMenu = document.querySelectorAll(".mobileMenu")[0];
     mobileMenu.style.opacity = "0";
     mobileMenu.style.transition = "200ms";
     mobileMenu.style.pointerEvents = "none";
-    let blur = document.getElementById("blur");
+    let blur = document.querySelectorAll(".blur")[0];
     blur.style.backdropFilter = "blur(0px)";
     blur.style.pointerEvents = "none";
 }
